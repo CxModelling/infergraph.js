ec919747315bfb3181af37035f4f4f3d6aac0b5e
TimeWZ667 <timewz667@gmail.com>
2018-04-18 11:33:12 +0100

    Separate node groups

--- src/graph.js
+++ src/graph.js
@@ -2,65 +2,7 @@
  * Created by TimeWz667 on 17/04/2018.
  */
 
-class NodeOp {
-    constructor(node) {
-        this.Node = node;
-    }
-
-    attr(name, value) {
-        if (arguments.length === 1) {
-            return this.Node[name];
-        }
-        if (value === null) {
-            delete this.Node[name];
-            return;
-        }
-
-        this.Node[name] = (typeof(value) === "function")? value(this.Node): value;
-
-        return this;
-    }
-
-    call(fn) {
-        return fn(this.Node);
-    }
-
-    get() {
-        return this.Node;
-    }
-
-    id() {
-        return this.Node.id;
-    }
-}
-
-
-class NodeOpGroup {
-    constructor(nodes) {
-        this.Nodes = nodes.map((node) => node instanceof NodeOp? node: new NodeOp(node));
-    }
-
-    attr(name, value) {
-        if (arguments.length === 1) {
-            return this.Nodes.map(node => node.attr(name));
-        }
-        this.Nodes.forEach(node => node.attr(name, value));
-
-        return this
-    }
-
-    call(fn) {
-        return this.Nodes.map(node => node.call(fn));
-    }
-
-    get() {
-        return this.Nodes.map(node => node.get());
-    }
-
-    id() {
-        return this.Nodes.map(node => node.id());
-    }
-}
+import {NodeOp, NodeOpGroup} from "./node";
 
 
 export class Graph {
